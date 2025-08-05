1da5bea0de064b84fc53c0873c61df0d43bc37f1
KangKyungHoon <knkhsu122@naver.com>
2025-08-05 21:29:41 +0900

    업로드

---
+++ GameServer/GameServer/GameServer.cpp
@@ -0,0 +1,59 @@
+﻿// GameServer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+#pragma comment(lib, "ws2_32.lib") //라이브러리 블러오기
+#include <winsock2.h> //소켓 서버 만들기 위해 필요
+
+int main()
+{
+    std::cout << "Game Server Hello World!\n";
+    
+    // 서버가 사용할 수 있는 windows 소캣 사양의 버전 설정
+    WORD wVersionRequested;
+
+    //windows 소캣 구현에 대한 세부 정보 받기 위해 사용
+    WSADATA wsaData;
+
+    //버전을 만들어서 넣어줌
+    wVersionRequested = MAKEWORD(2,2);
+
+    //winsock DLL 사용을 시작
+    if (WSAStartup(wVersionRequested, &wsaData) != 0)
+    {
+        //에러 뱉어 내고 끝
+        printf("winsock filed with error\n");
+        return 1;
+    }
+
+    //AF_INET : IPv4(인터넷 프로토콜 버전 4) 주소 패밀리입니다.
+    //AF_INET6 : IPv6(인터넷 프로토콜 버전 6) 주소 패밀리입니다.
+   
+    //SOCK_STREAM : TCP
+    //SOCK_DGRAM : UDP
+
+    //프로토콜  : 0 값이 0인 경우 자동으로 할당
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET)
+    {
+        //에러 발생시 어떤 에러가 발생 했는지 확인
+        printf("socket error :%d\n", WSAGetLastError());
+        return 1;
+    }
+    else
+    {
+        // 성공적으로 소캣 성생 출력
+        printf("socket creation\n");
+    }
+    
+    //sockaddr_in 구조체 연동
+    //    SOCKADDR_IN service  = {}; 0으로 초기화 해도됨
+    SOCKADDR_IN service = {};
+
+    closesocket(sock);
+    //winsock DLL 종료
+    WSACleanup();
+}
+
